a4d97cc4248da3537fd9170e60d3ed086de62d48
Jingjing Duan <jduan@twitter.com>
2015-08-23 22:15:49 -0700

    pythagorean_triplet

---
+++ pythagorean-triplet/pythagorean_triplet.exs
@@ -0,0 +1,47 @@
+defmodule Triplet do
+
+  @doc """
+  Calculates sum of a given triplet of integers.
+  """
+  @spec sum([non_neg_integer]) :: non_neg_integer
+  def sum(triplet) do
+    Enum.sum(triplet)
+  end
+
+  @doc """
+  Calculates product of a given triplet of integers.
+  """
+  @spec product([non_neg_integer]) :: non_neg_integer
+  def product(triplet) do
+    triplet
+    |> Enum.reduce(1, &(&1 * &2))
+  end
+
+  @doc """
+  Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
+  """
+  @spec pythagorean?([non_neg_integer]) :: boolean
+  def pythagorean?([a, b, c]) do
+    a * a + b * b == c * c
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min (or 1 if no min) to a given max.
+  """
+  @spec generate(non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max) do
+    for i <- min..max, j <- i..max, k <- j..max do
+      [i, j, k]
+    end
+    |> Enum.filter(&pythagorean?/1)
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min to a given max, whose values add up to a given sum.
+  """
+  @spec generate(non_neg_integer, non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max, sum) do
+    generate(min, max)
+    |> Enum.filter(fn lst -> Enum.sum(lst) == sum end)
+  end
+end
